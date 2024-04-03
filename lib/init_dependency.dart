18343c35bdf6ca363489ba225f525a212386f4e8
spandanmn <tyian3001@gmail.com>
2024-04-03 16:55:14 +0530

    feat: added supabase for project

---
+++ lib/init_dependency.dart
@@ -0,0 +1,12 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+}
